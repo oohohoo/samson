bae48d21d3cbb3152cd8ce9789a06cebaa9ef0ba
dejan-77 <dejankutic@me.com>
2021-01-08 19:09:45 +0100

    gfgf

--- samson-newapp.js
+++ samson-newapp.js
@@ -640,7 +640,7 @@ function fullscreenMenu() {
       })
 
       .fromTo(".f-nav", {
-        yPercent: 110,
+        yPercent: 130,
       }, {
         duration: 0.65,
         yPercent: 0,
@@ -685,7 +685,7 @@ function fullscreenMenu() {
 
     tl.to(".f-nav", {
         duration: 0.35,
-        yPercent: -110,
+        yPercent: -130,
         ease: "Expo.inOut",
         stagger: -0.15,
       }, 0)
