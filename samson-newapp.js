22cad7edf5bbf0f3a1052ca84b5262864d1dcee6
dejan-77 <dejankutic@me.com>
2021-01-09 11:58:13 +0100

    grgr

--- samson-newapp.js
+++ samson-newapp.js
@@ -638,11 +638,11 @@ function fullscreenMenu() {
       /*.fromTo(".f-img-mask",	{clipPath:"polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)"},
 		{duration:0.5, ease: "Expo.inOut", sclipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}) */
 
-      .fromTo(".f-img-mask", {
+     /* .fromTo(".f-img-mask", {
         clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)"}, 
         {autoAlpha: 1, duration: 0.55,y: 0,clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"})
-
-      .fromTo(".f-nav", {yPercent: 150,}, {duration: 0.65, yPercent: 0, stagger: 0.02,}, "<0.1")
+*/
+      .fromTo(".f-nav", {yPercent: 170,}, {duration: 0.65, yPercent: 0, stagger: 0.02,}, "<0.1")
       //.fromTo(".f-cap", {yPercent: 100,}, {duration: 0.65,yPercent: 0,stagger: 0.02,}, "<")
       .fromTo(".f-foot, .arch, .copy-right", {y: 20,autoAlpha: 0}, 
         {duration: 0.65,y: 0,autoAlpha: 1, ease: "Expo.inOut",
