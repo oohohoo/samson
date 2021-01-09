c00b4ca1b74c673448be240a8b9b56fb22224ae9
dejan-77 <dejankutic@me.com>
2021-01-09 20:31:39 +0100

    grh

--- samson-newapp.js
+++ samson-newapp.js
@@ -839,14 +839,15 @@ tl2.from(".open", {autoAlpha:0, rotate:360, scale: 0.1})
 
 
 open.addEventListener('click', () => {
-tl.restart(), 
+//tl.restart(), 
 tl.play();
 console.log("MENU PLAY");
 })
 
 close.addEventListener('click', () => {
   //tl2.restart(), 
-  tl2.play();
+  tl.reverse(), 
+  //tl2.play();
   console.log("MENU 2 PLAY aaaa");
   })
 
