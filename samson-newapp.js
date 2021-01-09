799085ccb9e136febef2fcfdda849479a6ccfed5
dejan-77 <dejankutic@me.com>
2021-01-09 12:58:08 +0100

    ggggggg

--- samson-newapp.js
+++ samson-newapp.js
@@ -842,7 +842,7 @@ function killMenu() {
     pointerEvents: "none",
   });
 
-  tl.to(".f-nav", {duration: 0.65,yPercent: -180,force3D:true,ease: "Expo.out",}, 0)
+  tl.to(".f-nav", {duration: 0.45,yPercent: -180,force3D:true,ease: "Expo.out",}, 0)
     .fromTo(".f-foot, .arch, .copy-right", {y: 0, autoAlpha: 1}, 
       {duration: 0.35,y: -20, autoAlpha: 0, ease: "none"}, "<0.1")
     .to(".nav--transition-slide", {duration: 0.65,transformOrigin: "top center", scaleY: 0, ease: "Expo.inOut",})
