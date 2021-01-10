cb4c9683c23527e6daf7fd1d199297215a58614d
dejan-77 <dejankutic@me.com>
2021-01-10 19:50:44 +0100

    ge

--- samson-newapp.js
+++ samson-newapp.js
@@ -746,8 +746,8 @@ menuOut.from(".open", {autoAlpha:0, rotate:360, scale: 0.1})
 .set(burger, {pointerEvents: "none"})
 
 .to(fnav, {duration: 0.65, yPercent: -110,ease: "Expo.inOut"}, 0)
-.to(slide, {duration: 0.35,transformOrigin: "top center", scaleY: 0, ease: "Expo.inOut",})
 .to(".fadein", {y: 20, autoAlpha: 0, duration: 0.65, ease: "Expo.inOut"})
+.to(slide, {duration: 0.35,transformOrigin: "top center", scaleY: 0, ease: "Expo.inOut",})
 
 .set(burger, {pointerEvents: "all"})
 //.set(fnav, {yPercent:50})
