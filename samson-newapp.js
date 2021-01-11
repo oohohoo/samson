b8fb31b7a0f7f2d2300cfdec70b301ece25f581d
dejan-77 <dejankutic@me.com>
2021-01-11 16:13:26 +0100

    feokfoek

--- samson-newapp.js
+++ samson-newapp.js
@@ -740,7 +740,7 @@ menuIn.to(".open", {autoAlpha:1, rotate:720, scale: 0.1})
 // MENU OUT  ANIMATION
 ----------------------------- */
   
-let menuOut = gsap.timeline({paused: true, reversed:true});
+let menuOut = gsap.timeline({paused: true});
   
 menuOut.from(".open", {autoAlpha:0, scale: 0.1})
 .to(".close", {autoAlpha:0, rotate:360, scale: 0.1}, "<")
