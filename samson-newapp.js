5008db224b6e5aa3c1e21ca6eb5026024ad1c5b3
dejan-77 <dejankutic@me.com>
2021-03-13 05:47:34 +0100

    ge

--- samson-newapp.js
+++ samson-newapp.js
@@ -750,7 +750,7 @@ menuOut
 
 .to(".f-nav", {duration: 0.3, yPercent: -110, stagger: 0.05, force3D: true}, "<0.1")
 .to(".fadein", {y: -20, autoAlpha: 0, duration: 0.35, ease: "Expo.inOut"}, "<0.2")
-.to(slide, {duration: 0.45,transformOrigin: "top center", scaleY: 0, ease: "Expo.inOut"}, "<0.3")
+.to(slide, {duration: 0.3,transformOrigin: "top center", scaleY: 0, ease: "Expo.inOut"}, "<0.3")
 
 .set(burger, {pointerEvents: "all"})
 
