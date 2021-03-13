19d4ec2508a4e6f09d35d49aaa0f2b3367fd8cec
dejan-77 <dejankutic@me.com>
2021-03-13 05:54:46 +0100

    j

--- samson-newapp.js
+++ samson-newapp.js
@@ -740,7 +740,7 @@ menuIn
 // MENU OUT  ANIMATION
 ----------------------------- */
   
-let menuOut = gsap.timeline({});
+let menuOut = gsap.timeline({reversed: true});
   
 menuOut
 .to(".open", {autoAlpha:1})
