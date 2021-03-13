fca21b06acf3fdb6e2ea121b39a41241a4cff526
dejan-77 <dejankutic@me.com>
2021-03-13 13:03:59 +0100

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -734,6 +734,12 @@ var toggle = true;
 //menuIn
 
 menuAnimation
+ .set(slide, {scaleY: 0, transformOrigin: "bottom center"})
+.set(".f-nav", {yPercent: 185})
+.set(mainwrap, {scaleY: 0})
+.set(".fadein", {y: 20, autoAlpha:0}) 
+
+
 .to(".open", {rotate:-45, autoAlpha:0})
 .from(".close", {autoAlpha:0, rotate:0}, "<")
   /* .to(navMain, {xPercent: 100, duration: 0.8, ease: "Expo.inOut"},0) */
@@ -769,10 +775,7 @@ menuAnimationBack
 
 .set(burger, {pointerEvents: "all"})
 
-/* .set(slide, {scaleY: 0, transformOrigin: "bottom center"})
-.set(".f-nav", {yPercent: 185})
-.set(mainwrap, {scaleY: 0})
-.set(".fadein", {y: 20, autoAlpha:0}) */
+
 
 ///////////////////////menuIn.invalidate();
 /* -----------------------------
