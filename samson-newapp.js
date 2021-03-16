dcc97c4290f2cbd731583267724cd23e2e3595c4
dejan-77 <dejankutic@me.com>
2021-03-16 14:40:06 +0100

    d

--- samson-newapp.js
+++ samson-newapp.js
@@ -735,7 +735,7 @@ var toggle = true;
 //menuIn
 
 menuAnimation
-.to(".open", {autoAlpha:1, rotate:-45})
+.to(".open", {autoAlpha:1, rotate:-90})
 .from(".close", {autoAlpha:0 })
   /* .to(navMain, {xPercent: 100, duration: 0.8, ease: "Expo.inOut"},0) */
 
