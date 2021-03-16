5da9023d14096351c470efdb9ed65d3c96228285
dejan-77 <dejankutic@me.com>
2021-03-16 16:01:43 +0100

    ge

--- samson-newapp.js
+++ samson-newapp.js
@@ -744,7 +744,7 @@ menuAnimation
 .set(burger, {pointerEvents: "none"}, "<")
 .to(mainwrap, {scaleY: 1, duration: 0.1}, "<0.1")
 .fromTo(slide, {scaleY: 0, transformOrigin: "bottom center"}, {duration: 0.45, scaleY: 1, ease: "Expo.inOut" }, "<0.2") 
-.fromTo(".f-nav", {yPercent: 185}, {duration: 0.65, yPercent: 0, stagger: 0.2}, "<0.3")
+.fromTo(".f-nav", {yPercent: 185}, {duration: 0.65, yPercent: 0, stagger: 0.1}, "<0.3")
 .fromTo(".fadein", {y: 20, autoAlpha: 0}, {duration: 0.65, y: 20, autoAlpha: 1, ease: "Expo.inOut"}, "<0.4")
 
 .set(burger, {pointerEvents: "all"})
