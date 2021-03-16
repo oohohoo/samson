bd11483954ba04f6f4e297c12d2778155ff24ecd
dejan-77 <dejankutic@me.com>
2021-03-16 15:01:22 +0100

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -760,9 +760,9 @@ menuOut */
 menuAnimationBack
   /* .to(navMain, {xPercent: 0, duration: 0.5,  ease: "Expo.inOut"},0) */
 .set(".open", {rotate:0})
-.to(".close", {autoAlpha:0, rotate:22.5})
+.to(".close", {autoAlpha:1, rotate:22.5})
 .to(".open", {autoAlpha:1}, "<")
-
+.to(".close", {autoAlpha:0})
 
 
 
