3a9fa7bdaf2e20d996102b418c1cc20b6e0e6824
dejan-77 <dejankutic@me.com>
2021-03-17 17:52:28 +0100

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -1078,7 +1078,7 @@ function projectsHeroAnima() {
   var heroanimation = gsap.timeline({defaults:{duration:1.5, ease: "power3.inOut"}})
   heroanimation
   //.set(".hide", {autoAlpha:1})
-  .from(".char", {stagger:0.02, yPercent:190})
+  .from(".char", {stagger:0.02, yPercent:200})
   .fromTo(".inmask", {y:400, autoAlpha:0, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
   {autoAlpha:1, duration:3, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")
   .fromTo(".fadein", {autoAlpha:0, y:40},{duration:0.6, autoAlpha:1, stagger:0.2, y:0}, "<0.1")
