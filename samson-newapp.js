19a9903213a5919c3174c2ccbf1a2b4de340abb8
dejan-77 <dejankutic@me.com>
2021-03-20 15:38:58 +0100

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -742,7 +742,7 @@ function fullscreenMenuNew() {
 //  let menuIsOpen = false; // tracks state
 
 /*/////*/
-var menuAnimation = gsap.timeline({paused:true, reversed:true});
+var menuAnimation = gsap.timeline({paused:true});
 var menuAnimationBack = gsap.timeline({paused:true});
 var navMain = document.getElementById("nav-main");
 var menuButton = document.getElementById("menu-button");
