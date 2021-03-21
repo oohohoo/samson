fe92297bbbd2f8f33023b7571cbe59e463926174
dejan-77 <dejankutic@me.com>
2021-03-21 14:41:10 +0100

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -913,9 +913,9 @@ function projectsHeroAnima() {
 
   Splitting();
   
-  var heroanimation = gsap.timeline({defaults:{duration:1.3, ease: "power3.inOut"}})
+  var heroanimation = gsap.timeline({defaults:{duration:1.5, ease: "power3.inOut"}})
   heroanimation
-  .set(".hide", {autoAlpha:1})
+  /* .set(".hide", {autoAlpha:1}) */
   .from(".char", {stagger:0.02, yPercent:125},"<")
   .fromTo(".in1", {autoAlpha:0, y:40},{duration:0.8,autoAlpha:1, y:0}, "<0.6")
   .fromTo(".in2", {autoAlpha:0, y:40},{duration:0.6, autoAlpha:1, stagger:0.2, y:0}, "<0.1")
@@ -939,11 +939,10 @@ function tailoredHeroAnima() {
   heroanimation
   //.set(".hide", {autoAlpha:1})
   .from(".char", {stagger:0.02, yPercent:125})
-  .fromTo(".t1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.05, y:0}, "<0.1")
+  .fromTo(".t1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.2, y:0}, "<0.6")
   .fromTo(".clip", {y:400, autoAlpha:0, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
-  {autoAlpha:1, duration:3, stagger:0.02, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")
+  {autoAlpha:1, duration:3, stagger:0.02, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "-=2")
 
-  console.log("MATERIALLLLLSSSSSS!!!!")
   
 }
 
