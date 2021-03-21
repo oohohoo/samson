19236c542695d1230d0c446ff1cef6c5000dae02
dejan-77 <dejankutic@me.com>
2021-03-21 15:12:16 +0100

    g

--- samson-newapp.js
+++ samson-newapp.js
@@ -959,10 +959,11 @@ var heroanimation = gsap.timeline({defaults:{duration:1.5, ease: "power3.inOut"}
 heroanimation
 //.set(".hide", {autoAlpha:1})
 .from(".char", {stagger:0.02, yPercent:125})
+/* .fromTo(".t1", {autoAlpha:0, y:100},{duration:0.8, autoAlpha:1, stagger:0.2, y:0}, "<0.6") */
 /*.fromTo(".clipImg", {y:400, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
 {autoAlpha:1, duration:3, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")*/
-.fromTo(".fadein", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.05, y:0}, "<1.2")
-console.log("IMAGE CONTACT CLIPPED NOW!!!!")
+.fromTo(".c1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.05, y:0}, "<1.2")
+
 
 }
  
