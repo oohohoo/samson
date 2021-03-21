20078d604d8cc12e330fe1e6873dc037e52e8ce8
dejan-77 <dejankutic@me.com>
2021-03-21 15:17:18 +0100

    ht

--- samson-newapp.js
+++ samson-newapp.js
@@ -962,7 +962,7 @@ heroanimation
 /* .fromTo(".t1", {autoAlpha:0, y:100},{duration:0.8, autoAlpha:1, stagger:0.2, y:0}, "<0.6") */
 /*.fromTo(".clipImg", {y:400, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
 {autoAlpha:1, duration:3, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")*/
-.fromTo(".c1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.2, y:0}, "<0.6")
+.fromTo(".c1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.02, y:0}, "<0.6")
 
 
 }
