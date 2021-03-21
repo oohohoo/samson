40f6cc016869e09b64591980867ee201cd74dabd
dejan-77 <dejankutic@me.com>
2021-03-21 10:28:57 +0100

    gd

--- samson-newapp.js
+++ samson-newapp.js
@@ -935,10 +935,10 @@ function projectsHeroAnima() {
   
   var heroanimation = gsap.timeline({defaults:{duration:1.3, ease: "power3.inOut"}})
   heroanimation
-  //.set(".hide", {autoAlpha:1})
+  .set(".hide", {autoAlpha:1})
   .from(".char", {stagger:0.02, yPercent:125})
-  .fromTo(".in1", {autoAlpha:0, y:40},{duration:0.5, autoAlpha:1, y:0}, "<0.2")
-  .fromTo(".in2", {autoAlpha:0, y:40},{duration:0.5, autoAlpha:1, stagger:0.02, y:0}, "<0.2")
+  .fromTo(".in1", {autoAlpha:0, y:40},{duration:0.5, autoAlpha:1, y:0}, "<2")
+  .fromTo(".in2", {autoAlpha:0, y:40},{duration:0.5, autoAlpha:1, stagger:0.2, y:0}, "<0.1")
  /*  .fromTo(".fadein", {autoAlpha:0, y:40},{duration:0.6, autoAlpha:1, stagger:0.2, y:0}, "<0.1") */
   .fromTo(".clip", {y:200, autoAlpha:0, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
   {autoAlpha:1, duration:2, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"},"-=1")
