9294bb9ef8dd84212e06c749ac6dc64c65a7a098
dejan-77 <dejankutic@me.com>
2021-03-21 07:45:06 +0100

    f

--- samson-newapp.js
+++ samson-newapp.js
@@ -944,7 +944,7 @@ function titleHero() {
  var animation = gsap.timeline({defaults:{duration:1.3, ease: "power3.inOut"}})
  animation
  .set(".hide", {autoAlpha:1}, "<")
- .from(".char", {stagger:0.02, yPercent:120}, "<")
+ .from(".char", {stagger:0.02, yPercent:125}, "<")
  .fromTo(".clip", {y:400, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
  {autoAlpha:1, duration:2.3, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")
  
@@ -1037,7 +1037,7 @@ Splitting();
 var heroanimation = gsap.timeline({defaults:{duration:1.5, ease: "power3.inOut"}})
 heroanimation
 //.set(".hide", {autoAlpha:1})
-.from(".char", {stagger:0.02, yPercent:120})
+.from(".char", {stagger:0.02, yPercent:125})
 /*.fromTo(".clipImg", {y:400, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
 {autoAlpha:1, duration:3, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")*/
 .fromTo(".fadein", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.05, y:0}, "<1.2")
@@ -1058,7 +1058,7 @@ function projectsHeroAnima() {
   var heroanimation = gsap.timeline({defaults:{duration:1.5, ease: "power3.inOut"}})
   heroanimation
   //.set(".hide", {autoAlpha:1})
-  .from(".char", {stagger:0.02, yPercent:120})
+  .from(".char", {stagger:0.02, yPercent:125})
   .fromTo(".clip", {y:400, autoAlpha:0, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
   {autoAlpha:1, duration:3, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")
   .fromTo(".fadein", {autoAlpha:0, y:40},{duration:0.6, autoAlpha:1, stagger:0.2, y:0}, "<0.1")
@@ -1078,7 +1078,7 @@ function materialsHeroAnima() {
   var heroanimation = gsap.timeline({defaults:{duration:1.5, ease: "power3.inOut"}})
   heroanimation
   //.set(".hide", {autoAlpha:1})
-  .from(".char", {stagger:0.02, yPercent:120})
+  .from(".char", {stagger:0.02, yPercent:125})
    .fromTo(".clip", {y:400, autoAlpha:0, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
   {autoAlpha:1, duration:3, stagger:0.02, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")
   .fromTo(".fadein", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.05, y:0}, "<0.1")
