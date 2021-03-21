d51ee77d02cbad718a26fefc10aa4a7f561c0c77
dejan-77 <dejankutic@me.com>
2021-03-21 09:59:53 +0100

    ht

--- samson-newapp.js
+++ samson-newapp.js
@@ -937,8 +937,8 @@ function projectsHeroAnima() {
   heroanimation
   //.set(".hide", {autoAlpha:1})
   .from(".char", {stagger:0.02, yPercent:125})
-  .fromTo(".in1", {autoAlpha:0, y:40},{autoAlpha:1, y:0}, "<0.1")
-  .fromTo(".in2", {autoAlpha:0, y:40},{autoAlpha:1, stagger:0.2, y:0}, "<0.1")
+  .fromTo(".in1", {autoAlpha:0, y:40},{autoAlpha:1, y:0}, "-=0.1")
+  .fromTo(".in2", {autoAlpha:0, y:40},{autoAlpha:1, stagger:0.2, y:0}, "-=0.1")
  /*  .fromTo(".fadein", {autoAlpha:0, y:40},{duration:0.6, autoAlpha:1, stagger:0.2, y:0}, "<0.1") */
   .fromTo(".clip", {y:400, autoAlpha:0, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
   {autoAlpha:1, duration:3, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")
