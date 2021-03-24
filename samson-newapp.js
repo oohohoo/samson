cf39120fbb36c9162222da47dfad51e782b87ab4
dejan-77 <dejankutic@me.com>
2021-03-24 16:41:23 +0100

    ge

--- samson-newapp.js
+++ samson-newapp.js
@@ -974,7 +974,7 @@ function tailoredHeroAnima() {
   .from(".char", {duration:1.5, stagger:0.02, yPercent:130})
   .fromTo(".t1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, y:0}, "<0.6")
   .fromTo(".clip", {y:200, autoAlpha:0, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
-  {autoAlpha:1, duration:2, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "-=2")
+  {autoAlpha:1, duration:2, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "-=1")
 
   
 }
