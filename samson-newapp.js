7e8cd38962c30554de3063384550a8b459567e73
dejan-77 <dejankutic@me.com>
2021-03-24 16:45:05 +0100

    ge

--- samson-newapp.js
+++ samson-newapp.js
@@ -931,8 +931,7 @@ function titleHero() {
  var animation = gsap.timeline({defaults:{ease: "power3.inOut"}})
  animation
  /* .set(".hide", {autoAlpha:1}, "<") */
- .from(".char", {stagger:0.02, yPercent:130, duration:1.3}, "<")
- .fromTo(".since", {autoAlpha:0, y:40},{duration:0.8,autoAlpha:1, y:0}, "<0.6")
+ .from(".char", {stagger:0.02, yPercent:130, duration:1.5}, "<")
  .fromTo(".clip", {y:200, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
  {autoAlpha:1, duration:2, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "-=1.8")
  }
@@ -991,11 +990,11 @@ Splitting();
 var heroanimation = gsap.timeline({defaults:{ease: "power3.inOut"}})
 heroanimation
 //.set(".hide", {autoAlpha:1})
-.from(".char", {stagger:0.02, duration:1.5,yPercent:125})
+.from(".char", {stagger:0.02, duration:1.5,yPercent:130})
 /* .fromTo(".t1", {autoAlpha:0, y:100},{duration:0.8, autoAlpha:1, stagger:0.2, y:0}, "<0.6") */
 /*.fromTo(".clipImg", {y:400, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
 {autoAlpha:1, duration:3, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")*/
-.fromTo(".c1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.02, y:0}, "<0.6")
+.fromTo(".c1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.02, y:0}, "<0.8")
 
 
 }
