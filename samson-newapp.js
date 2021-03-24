7b8c374a33f2d53f9d2248cede0c17baed031429
dejan-77 <dejankutic@me.com>
2021-03-24 16:51:01 +0100

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -932,8 +932,8 @@ function titleHero() {
  animation
  /* .set(".hide", {autoAlpha:1}, "<") */
  .from(".char", {stagger:0.02, yPercent:130, duration:1.5})
- .fromTo(".clip", {y:200, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
- {autoAlpha:1, duration:2, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "-=1.8")
+ .fromTo(".clip", {autoAlpha:1, y:200, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
+ { autoAlpha:1, duration:2, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "-=1.8")
  }
 
 /*
