57fc047f00a088a95252ae590ee7e4f13508123d
dejan-77 <dejankutic@me.com>
2021-03-24 16:29:13 +0100

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -855,7 +855,7 @@ dev.interactions = {
 
   filtersOnEnter: function(e) {
     // fade out all anchors
-    gsap.to("[data-filters] a", {duration:0.5, color: "#D0D0CE"});
+    gsap.to("[data-filters] a", {duration:0.3, color: "#D0D0CE"});
     
     // keep hovered anchor the same
     gsap.to(e.target, {color: "#1e1e1e", overwrite: true});
@@ -863,7 +863,7 @@ dev.interactions = {
 
   filtersOnLeave: function(e) {
     // animate all anchors to normal calue
-    gsap.to("[data-filters] a", {duration:0.5, color: "#1e1e1e"});
+    gsap.to("[data-filters] a", {duration:0.3, color: "#1e1e1e"});
   }
 };
 dev.interactions.init();
