fff63f09ebc0c71c2ae0cd0c68846368740693df
dejan-77 <dejankutic@me.com>
2021-03-24 14:34:55 +0100

    bhj

--- samson-newapp.js
+++ samson-newapp.js
@@ -885,7 +885,7 @@ $( "#tocom" ).on( "click", function() {
 
 $( "#tores" ).on( "click", function() {
   locoScroll.scrollTo( '#res', {
-    'offset': 0,
+    'offset': -150,
     'duration': 800,
     'easing': [0.25, 0.00, 0.35, 1.00],
     'disableLerp': true
