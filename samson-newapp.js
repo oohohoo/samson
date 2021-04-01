1cf58355346b01d9c380c0325ba14d32c7f934dd
dejan-77 <dejankutic@me.com>
2021-04-01 15:02:04 +0200

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -1020,15 +1020,25 @@ function contactHeroAnima() {
 
 Splitting();
 
+
+
+const mq = window.matchMedia('(min-width: 999px)')
+if (mq.matches) {
 var heroanimation = gsap.timeline({defaults:{ease: "power3.inOut"}})
 heroanimation
-//.set(".hide", {autoAlpha:1})
 .from(".char", {stagger:0.02, duration:1.5,yPercent:130})
-/* .fromTo(".t1", {autoAlpha:0, y:100},{duration:0.8, autoAlpha:1, stagger:0.2, y:0}, "<0.6") */
-/*.fromTo(".clipImg", {y:400, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
-{autoAlpha:1, duration:3, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "<0.1")*/
 .fromTo(".c1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, stagger:0.02, y:0}, "-=1.2")
-3
+
+
+}
+else {
+  var heroanimation = gsap.timeline({defaults:{ease: "power3.inOut"}})
+  heroanimation
+.set(".c1", {autoAlpha:1} );
+}
+
+
+
 
 }
  
