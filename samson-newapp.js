f0d51f1a213a86fb15d4fd6f82d1f3c237c03676
dejan-77 <dejankutic@me.com>
2021-04-05 22:37:22 +0200

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -377,7 +377,7 @@ function initLoader() {
   tlLoaderOut
 
     .to(lines, {yPercent: -500,stagger: 0.2}, 0)
-    .to([loader, loaderContent], {yPercent: -200})
+    .to([loader, loaderContent], {yPercent: -100})
     //.to('.main', {y: 0}, 0);
 
   const tlLoader = gsap.timeline();
