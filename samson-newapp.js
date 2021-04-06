9b2943b75c5490f367984c34172b7ab8c86a672d
dejan-77 <dejankutic@me.com>
2021-04-06 19:27:50 +0200

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -380,7 +380,7 @@ function initLoader() {
 
   tlLoaderOut
 
-    .to(lines, {yPercent: -100}, 0)
+    .to(lines, {yPercent: -100,stagger: 0.3}, 0)
     .to([loader, loaderContent], {yPercent: -100}, '-=0.6')
     //.to('.main', {y: 0}, 0);
 
