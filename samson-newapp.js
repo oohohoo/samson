0628fd911f8b631c4484934cfa94c3b3714f3da2
dejan-77 <dejankutic@me.com>
2021-04-06 19:43:44 +0200

    gr

--- samson-newapp.js
+++ samson-newapp.js
@@ -381,6 +381,7 @@ function initLoader() {
   tlLoaderOut
 .to(".loggo", {autoAlpha:0}, 0)
     .to(lines, {stagger: 0.2, autoAlpha:0})
+    .to(".since", {autoAlpha:0})
     .to([loader, loaderContent], {yPercent: -100}, '-=0.5')
     //.to('.main', {y: 0}, 0);
 
