608f45850fa3207ca7273b6cd14e39431a06109b
dejan-77 <dejankutic@me.com>
2021-04-06 19:02:11 +0200

    ge

--- samson-newapp.js
+++ samson-newapp.js
@@ -365,7 +365,7 @@ function initLoader() {
     //.set(".main", {y: 150})
 
 
-    .to(loaderInner, {scaleX: 0,transformOrigin: 'left bottom',ease: 'power1.inOut'}) 
+    .to(loaderInner, {scaleX: 0,transformOrigin: 'right bottom',ease: 'power1.inOut'}) 
 
     .addLabel('revealImage')
     //.to(image, {yPercent: 0}, 'revealImage-=0.5') 
