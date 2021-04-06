2ebf21c88883b38235f3237ea992fabf479dc5d6
dejan-77 <dejankutic@me.com>
2021-04-06 20:31:14 +0200

    f

--- samson-newapp.js
+++ samson-newapp.js
@@ -1022,7 +1022,7 @@ if (mq.matches) {
   var heroanimation = gsap.timeline({defaults:{force3d: true, ease: "power3.inOut"}})
   heroanimation
   /* .set(".hide", {autoAlpha:1}) */
-  .from(".char", {duration:1.5, stagger:0.02, yPercent:130},"<")
+  .from(".char", {duration:1.5, stagger:0.02, yPercent:130})
   .fromTo(".in1", {autoAlpha:0, y:40},{duration:0.8,autoAlpha:1, y:0}, "<0.6")
   .fromTo(".in2", {autoAlpha:0, y:40},{duration:0.6, autoAlpha:1, stagger:0.2, y:0}, "<0.1")
   .fromTo(".clip", {y:200, autoAlpha:0, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
@@ -1056,8 +1056,8 @@ if (mq.matches) {
 var heroanimation = gsap.timeline({defaults:{ease: "power3.inOut"}})
   heroanimation
   //.set(".hide", {autoAlpha:1})
-  .from(".char", {duration:1.8, stagger:0.02, yPercent:130})
-  .fromTo(".t1", {autoAlpha:0, y:40},{duration:1.1, autoAlpha:1, y:0}, "<0.6")
+  .from(".char", {duration:1.5, stagger:0.02, yPercent:130})
+  .fromTo(".t1", {autoAlpha:0, y:40},{duration:0.8, autoAlpha:1, y:0}, "<0.6")
   .fromTo(".clip", {y:200, autoAlpha:0, clipPath:"polygon(40% 30%, 60% 30%, 60% 90%, 40% 90%)"}, 
   {autoAlpha:1, duration:2, y:0, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}, "-=2")
 
